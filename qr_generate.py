e3061737db6d40a278db1b452f266bd15ddf7e17
Grondi <125816111+Gr0ndi@users.noreply.github.com>
2023-04-20 12:25:37 +0200

    Initial commit

---
+++ qr_generate.py
@@ -0,0 +1,26 @@
+import qrcode
+import sys
+
+#print usage
+if len(sys.argv) == 1:
+    print("Usage: qr_generate.py [QR code text] [filename]")
+    sys.exit()
+
+#if one argument has been supplied, then it will be used both as QR code text and filename + .svg
+text = sys.argv[1]
+#if two arguments are supplied then the second argument will be used as a filename + .svg
+if len(sys.argv) == 2:
+    file_name = text + ".svg"
+else:
+    file_name = sys.argv[2] + ".svg"
+
+#generating qrcode
+qr = qrcode.QRCode(version=1, box_size=10, border=4)
+qr.add_data(text)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="transparent")
+img.save(file_name)
+
+print("QR code saved as:", file_name)
+
